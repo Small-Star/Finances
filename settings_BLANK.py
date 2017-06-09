28b89c284e1d939604e4a916554b6da5a164f1a8
Small-Star <small_star@gmx.com>
2017-06-08 21:01:46 -0400

    Added blank supplementary files

---
+++ settings_BLANK.py
@@ -0,0 +1,19 @@
+FILENAME=""
+LOG_FILENAME=""
+CATEGORIES_FILENAME="category_targets.json"
+TSP_PATH=""
+PAYSTUB_PATH=""
+PYOOCALCPATH=""
+
+TIME_INTERVAL=14 #How often to bin things (days)
+
+SHEET_ECT=0 #Index for Expense Category Tracking sheet
+SHEET_CEL=1 #Index for Cash Expense List sheet
+SHEET_CCEL=2 #Index for Credit Card Expense List sheet
+SHEET_OEL=3 #Index for Other Expense List sheet
+SHEET_INC=4 #Index for Income sheet
+SHEET_ICPNR=5 #Index for Investments (Non-Retirement Acct)
+SHEET_ICPR=6 #Index for Investments (Retirement Acct)
+SHEET_NW=7 #Index for Net Worth sheet
+
+
